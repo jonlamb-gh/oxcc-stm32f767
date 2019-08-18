972ed5ef91cb586e9d30b5cd75a9e05bc687f709
Jon Lamb <lamb.jon.io@gmail.com>
2019-08-18 06:29:37 -0700

    WIP changes for home-phone

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 #![allow(non_camel_case_types)]
-#![feature(const_fn)]
-#![feature(try_from)]
 #![no_std]
-#![cfg_attr(feature = "rt", feature(global_asm))]
-#![cfg_attr(feature = "rt", feature(use_extern_macros))]
-#![cfg_attr(feature = "rt", feature(used))]
+//#![cfg_attr(feature = "rt", feature(global_asm))]
+//#![cfg_attr(feature = "rt", feature(use_extern_macros))]
+//#![cfg_attr(feature = "rt", feature(used))]
 
 extern crate bare_metal;
 extern crate cortex_m;
